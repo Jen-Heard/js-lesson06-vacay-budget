31c91a1a7c8d2560fdcaffa71dbcb26328599cfb
Jen-Heard <jlheard11@gmail.com>
2024-12-08 13:52:22 -0600

    Deleted and recompleted all project steps

--- js/script.js
+++ js/script.js
@@ -2,17 +2,17 @@ var tripInfoButton = document.querySelector(".trip-info");
 var dailyBudget = document.querySelector(".daily-budget");
 
 var tripInfo = function () {
-  var totalBudget = Number(prompt("What is your total budget for this trip? "));
-  var accommodation = Number(prompt("What are your accommodation costs? "));
-  var numDays = Number(prompt("How many days does your trip last? "));
-
+  var totalBudget = Number(prompt("What is your total budget?"));
+  var accommodation = Number(prompt("What are your accommodation costs?"));
+  var numDays = Number(prompt("How many days does your trip last?"));
+  
   calculateDailyBudget(totalBudget, accommodation, numDays);
 };
 
 var calculateDailyBudget = function (totalBudget, accommodation, numDays) {
   var daily = ((totalBudget - accommodation) / numDays).toFixed(2);
-
-  dailyBudget.innerText = `You can spend $${daily} a day on food and fun!`;
+  
+  dailyBudget.innerText = `You can spend $${daily} on food and fun!`;
 };
 
 tripInfoButton.addEventListener("click", tripInfo);
